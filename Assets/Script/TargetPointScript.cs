c6b075f420f4836bd014871c9e3b5a00c87a5d45
WindowsNT40 <nikitapavlov1109@gmail.com>
2023-12-18 17:24:30 +0200

    TargetPoint_AAG

---
+++ Assets/Script/TargetPointScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetPointScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject Drone;
+
+    public GameObject GetDrone()
+    {
+        return Drone;
+    }
+
+
+
+
+}
