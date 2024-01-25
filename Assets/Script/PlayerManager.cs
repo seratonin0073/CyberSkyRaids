c8367d9d74da27719c6982bd4633ce9e3baac02f
WindowsNT40 <nikitapavlov1109@gmail.com>
2024-01-25 20:23:02 +0200

    Round1

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -5,6 +5,7 @@ using System.IO;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerManager : MonoBehaviourPunCallbacks
 {
@@ -21,6 +22,16 @@ public class PlayerManager : MonoBehaviourPunCallbacks
     [SerializeField] private GameObject Drone;
 
     [SerializeField] private GameObject Hungar;
+
+    private int countRound = 0;
+
+    private int scoreAA = 0;
+
+    [SerializeField] private TextMeshProUGUI ScoreA;
+
+    [SerializeField] private TextMeshProUGUI ScoreD;
+
+    [SerializeField] private GameObject UI;
 
 
     bool clients = false;
@@ -55,6 +66,11 @@ public class PlayerManager : MonoBehaviourPunCallbacks
 
 
             
+        }
+
+        if (!photonView.IsMine)
+        {
+            Destroy(UI);
         }
 
         isStart = true;
@@ -107,23 +123,40 @@ public class PlayerManager : MonoBehaviourPunCallbacks
             if (!photonView.Owner.IsMasterClient && AA == null && PhotonNetwork.NickName != "FreeCam")
             {
                 AA = PhotonNetwork.Instantiate(Path.Combine("sci-Fi-Car"), AASpawn[Random.Range(0, AASpawn.Length - 1)].transform.position, Quaternion.identity);
+                countRound++;
+                photonView.RPC(nameof(SendDrone), RpcTarget.All);
+
             }
             if (photonView.Owner.IsMasterClient && Drone == null)
             {
                 Drone = PhotonNetwork.Instantiate(Path.Combine("Sci-fi-Plane (1)"),
                  DroneSpawn[Random.Range(0, DroneSpawn.Length - 1)].transform.position, DroneSpawn[Random.Range(0, DroneSpawn.Length - 1)].transform.rotation);
+                scoreAA++;
+                countRound++;
+                photonView.RPC(nameof(SendAA), RpcTarget.All);
+
+               
+
             }
 
             if (Hungar == null && (!photonView.Owner.IsMasterClient && PhotonNetwork.NickName != "FreeCam"))
             {
                 photonView.RPC(nameof(LeaveOnRound), RpcTarget.All);
                 LeaveOnRound();
+
+               
+
             }
+
+            ScoreA.text = scoreAA.ToString();
+
+            ScoreD.text = (countRound - scoreAA).ToString();
+
 
 
         }
 
-       
+
 
 
 
@@ -134,10 +167,31 @@ public class PlayerManager : MonoBehaviourPunCallbacks
 
     public void LeaveOnRound()
     {
-        PhotonNetwork.LeaveRoom();
-        SceneManager.LoadScene("Lobbi");
+        
 
-        Cursor.lockState = CursorLockMode.None;
+
+        if(countRound == 5)
+        {
+           
+
+
+
+        }
+
+    }
+
+    [Photon.Pun.PunRPC]
+    public void SendAA()
+    {
+        scoreAA++;
+        countRound++;
+    }
+
+
+    [Photon.Pun.PunRPC]
+    public void SendDrone()
+    {
+        countRound++;
     }
 
 
